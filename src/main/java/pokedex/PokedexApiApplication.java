1d63e5708cbae65f5e460fcdcd680217f212cd23
Neill Johnson <censhin@gmail.com>
2016-03-19 18:00:51 -0500

    feat(all): add gradle with build and autogenerated files

---
+++ src/main/java/pokedex/PokedexApiApplication.java
@@ -0,0 +1,12 @@
+package pokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PokedexApiApplication.class, args);
+  }
+}
