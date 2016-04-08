1a9cdb5eecbf93c0eacdd1454b9a2bb84f79e37e
Neill Johnson <censhin@gmail.com>
2016-04-08 00:06:10 -0500

    fix(member resource): fix imports that IDE auto changed

--- src/main/java/pokedex/PokemonMemberResource.java
+++ src/main/java/pokedex/PokemonMemberResource.java
@@ -1,6 +1,10 @@
 package pokedex;
 
-import javax.ws.rs.*;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 @Path("pokemon/{name}")
 public class PokemonMemberResource {
